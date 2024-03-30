67dc17c25b571eddcafd1af3e1bf63654847ce2e
ChromMob <filip.chromy04@gmail.com>
2024-03-30 21:43:24 +0100

    Forgot to add fakeplayerapi as dependency

--- src/main/java/net/elytrium/fastmotd/FastMOTD.java
+++ src/main/java/net/elytrium/fastmotd/FastMOTD.java
@@ -23,6 +23,7 @@ import com.velocitypowered.api.event.EventManager;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.network.ProtocolVersion;
+import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
@@ -77,6 +78,8 @@ import org.slf4j.Logger;
 
 @Plugin(id = "fastmotd", name = "FastMOTD", version = BuildConstants.VERSION, description = "MOTD plugin that uses FastPrepareAPI.", url = "https://elytrium.net/", authors = {
     "Elytrium (https://elytrium.net/)",
+}, dependencies = {
+    @Dependency(id = "fakeplayerapi"),
 })
 public class FastMOTD {
 
